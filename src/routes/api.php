43645fcf8d8a2cf21d17bd89e614555cca95ab0b
kodaishiotsuki <0711dr5@gmail.com>
2022-06-16 18:09:30 +0900

    認証承認ユーザー機能

--- src/routes/api.php
+++ src/routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ForgetController;
 use App\Http\Controllers\ResetController;
+use App\Http\Controllers\UserController;
 
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //     return $request->user();
@@ -22,3 +23,6 @@ Route::post('/forgetpassword', [ForgetController::class, 'ForgetPassword']);
 
 //reset password Route
 Route::post('/resetpassword', [ResetController::class, 'ResetPassword']);
+
+//Current User Route
+Route::get('/user', [UserController::class, 'User'])->middleware('auth:api'); //認証後に処理開始
