535ec2ac0e96d4d43063a37f7f263884c4bb5b2b
kodaishiotsuki <0711dr5@gmail.com>
2022-06-16 15:16:05 +0900

    laravel passport ログイン

--- src/app/Http/Controllers/AuthController.php
+++ src/app/Http/Controllers/AuthController.php
@@ -3,8 +3,33 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use Auth;
+use DB;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
-    //
+    public function Login(Request $request)
+    {
+        try {
+            if (Auth::attempt($request->only('email', 'password'))) {
+                $user = Auth::user();
+                $token = $user->createToken('app')->accessToken;
+
+                return response([
+                    'message' => 'Login successful',
+                    'token' => $token,
+                    'user' => $user
+                ], 200);
+            }
+        } catch (Exception $exception) {
+            return response([
+                'message' => $exception->getMessage(),
+            ], 400);
+        }
+        return response([
+            'message' => 'Invalid Email Or Password',
+        ], 401);
+    }
 }
